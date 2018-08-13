441c9bdbc934c948db53f53babdf338af56aee8c
NateNeumann <natejneumann@gmail.com>
2018-08-13 15:47:43 +0900

    added proxy in to redirect around ors

--- src/post-script.js
+++ src/post-script.js
@@ -36,7 +36,7 @@ $("#getIdButton").click(function() {
   let authCode = $("input[name=authcode]").val();
   $.ajax({
     type: "GET",
-    url: "https://api.medium.com/v1/me",
+    url: "https://cors-anywhere.herokuapp.com/https://api.medium.com/v1/me",
     headers: {
       Authorization:
         "Bearer 2a515fe743d0ea3327a9140ac3e9e235aab6d7ce57a34d7632fd7edcd9b5460e3",
