662943ddc8a6a7c3c5b3d2589e120e0f693aa58c
NateNeumann <natejneumann@gmail.com>
2018-08-14 14:58:01 +0900

    restored id function, limits form to the actual form

--- src/post-script.js
+++ src/post-script.js
@@ -1,7 +1,7 @@
 // import { $, jQuery } from "jquery";
 let markdownFile, authCode, userId, errorDiv;
 
-document.forms["postform"].elements["mdfile"].onchange = function(evt) {
+document.getElementById("mdfile").onchange = function(evt) {
   if (!window.FileReader) return; // Browser is not compatible
 
   var reader = new FileReader();
@@ -15,7 +15,7 @@ document.forms["postform"].elements["mdfile"].onchange = function(evt) {
 
     filecontent = evt.target.result;
 
-    document.forms["postform"].elements["rawmd"].value = evt.target.result;
+    document.getElementById("rawmd").value = evt.target.result;
 
     document.getElementById("postpreview").innerHTML = marked(
       evt.target.result
@@ -25,16 +25,41 @@ document.forms["postform"].elements["mdfile"].onchange = function(evt) {
   reader.readAsText(evt.target.files[0]);
 };
 
-document.forms["postform"].elements["rawmd"].onchange = function(evt) {
+document.getElementById("rawmd").onchange = function(evt) {
   document.getElementById("postpreview").innerHTML =
-    "Post Preview: \n" +
-    marked(document.forms["postform"].elements["rawmd"].value);
+    "Post Preview: \n" + marked(document.getElementById("rawmd").value);
 };
+
+$("#getIdButton").click(function() {
+  authCode = document.getElementById("authcode").value;
+  $.ajax({
+    type: "GET",
+    url: "https://cors-anywhere.herokuapp.com/https://api.medium.com/v1/me",
+    headers: {
+      Authorization: "Bearer " + authCode,
+      contentType: "application/json;charset=utf-8",
+      Accept: "application / json",
+      acceptCharset: "utf-8"
+    },
+    success: function(response) {
+      console.log(response);
+      userId = response.data.id;
+      errorDiv = document.getElementById("error-message");
+      errorDiv.innerHTML = "Ready to go!";
+    },
+    error: function(xhr, status, error) {
+      var err = eval("(" + xhr.responseText + ")");
+      console.log(err);
+      errorDiv = document.getElementById("error-message");
+      errorDiv.innerHTML = err;
+    }
+  });
+});
 
 $("#submitPostOrgButton").click(function() {
   userId = document.getElementById("userId").value;
   let postTitle = $("input[name=title]").val();
-  let markdownFile = document.forms["postform"].elements["rawmd"].value;
+  let markdownFile = document.getElementById("rawmd").value;
 
   $.ajax({
     type: "POST",
